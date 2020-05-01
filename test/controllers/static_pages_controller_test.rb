c9b36e3704eccfd3bf1ea382f20df5fe6524c6be
Céline Le Tutour <celinelt@macbook-pro-de-celine.home>
2020-05-01 17:57:48 +0200

    balblab

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,24 +5,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Au bout du monde"
+    assert_select "title", "Au bout du monde"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Au bout du monde"
+    assert_select "title", "Au bout du monde"
   end
 
   test "should get about" do
   	get static_pages_about_url
   	assert_response :success
-  	assert_select "title", "About | Au bout du monde"
+  	assert_select "title", "Au bout du monde"
   end 
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact | Au bout du monde"
+    assert_select "title", "Au bout du monde"
   end 
 end
