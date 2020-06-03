647196ce8365642b32d9111f99a88d4702057e06
Céline Le Tutour <celinelt@macbook-pro-de-celine.home>
2020-06-03 19:34:43 +0200

    finish user edit, update,index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -36,4 +36,16 @@ def log_out
 	session.delete(:user_id)
 	@current_user = nil
 end 
+
+def current_user?(user)
+	user && user == current_user
+end 
+def redirect_back_or(default)
+	redirect_to(session[:forwarding_url] || default)
+    session.delete(:fowarding_url)
+end
+
+def store_location
+	session[:forwarding_url] = request.original_url if request.get?
+end 
 end
